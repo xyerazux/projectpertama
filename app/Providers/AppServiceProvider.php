15f503b80c0390e05614e8d7919f3a291bb0d589
xyerazux <akhtarpribd67@gmail.com>
2026-02-10 15:58:26 +0700

    sementara

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
             $view->with('categories', $categories);
         }
     });
+
+    \Illuminate\Support\Facades\Artisan::call('config:clear');
 }
 
 }
