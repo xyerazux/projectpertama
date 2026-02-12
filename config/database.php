848cc96f9ecb8263d1dd73059462396338470068
xyerazux <akhtarpribd67@gmail.com>
2026-02-12 09:22:49 +0700

    Force database config to read env

--- config/database.php
+++ config/database.php
@@ -19,10 +19,10 @@ return [
        'mysql' => [
     'driver' => 'mysql',
     'url' => env('DATABASE_URL'),
-    'host' => env('DB_HOST', '127.0.0.1'),
-    'port' => env('DB_PORT', '3306'),
-    'database' => env('DB_DATABASE', 'forge'),
-    'username' => env('DB_USERNAME', 'forge'),
+    'host' => env('DB_HOST', '127.0.0.1'), // Ini akan mengambil ${{MYSQLHOST}} dari Railway
+    'port' => env('DB_PORT', '3306'),      // Ini akan mengambil ${{MYSQLPORT}} dari Railway
+    'database' => env('DB_DATABASE', 'laravel'),
+    'username' => env('DB_USERNAME', 'root'),
     'password' => env('DB_PASSWORD', ''),
     'unix_socket' => env('DB_SOCKET', ''),
     'charset' => 'utf8mb4',
