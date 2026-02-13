3d4fc133b0c8d9edb9062817c25976f570b514e9
xyerazux <akhtarpribd67@gmail.com>
2026-02-13 16:49:49 +0700

    fix edit

---
+++ database/migrations/2026_02_13_094850_add_link_attachment_to_tasks_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::table('tasks', function (Blueprint $table) {
+        if (!Schema::hasColumn('tasks', 'link_attachment')) {
+            $table->string('link_attachment')->nullable()->after('description');
+        }
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
+    }
+};
